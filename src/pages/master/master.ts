bb5089acd0137e5a7c094f59be0c6b47812433c6
ryan10328 <ryan10328@gmail.com>
2017-06-03 00:55:08 +0800

    6. 在 MasterPage 中實作 goHome 方法，並透過 Pop 方式回到上一頁

--- src/pages/master/master.ts
+++ src/pages/master/master.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MasterPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-master',
@@ -15,6 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MasterPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  goHome() {
+    this.navCtrl.pop();
   }
 
   ionViewDidLoad() {
